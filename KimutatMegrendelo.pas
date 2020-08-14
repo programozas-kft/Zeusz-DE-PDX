6927beb90fc050c191078cda1a20d89ed86d9210
Kévés János <programozas.kft@gmail.com>
2020-08-14 10:36:10 +0200

    Add files via upload

---
+++ KimutatMegrendelo.pas
@@ -0,0 +1,29 @@
+unit KimutatMegrendelo;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ComCtrls;
+
+type
+  TKimutatMegrendeloo = class(TForm)
+    Label1: TLabel;
+    DateTimePicker1: TDateTimePicker;
+    DateTimePicker2: TDateTimePicker;
+    Label2: TLabel;
+    ComboBox1: TComboBox;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  KimutatMegrendeloo: TKimutatMegrendeloo;
+
+implementation
+
+{$R *.DFM}
+
+end.
